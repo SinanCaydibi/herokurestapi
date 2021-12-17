2a959381c6826cd79a7719dba5a5cf1ad78f72b5
SinanCaydibi <67194575+SinanCaydibi@users.noreply.github.com>
2021-12-17 13:56:00 +0200

    Update settings.py

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -77,18 +77,23 @@ WSGI_APPLICATION = 'restapi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'HerokuDb',
-        'USER': 'postgres',
-        'PASSWORD' : '1234',
-        'HOST' : 'localhost',
-        'PORT': '5432'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'HerokuDb',
+#         'USER': 'postgres',
+#         'PASSWORD' : '1234',
+#         'HOST' : 'localhost',
+#         'PORT': '5432'
+
+#     }
+# }
 
 
 # Password validation
